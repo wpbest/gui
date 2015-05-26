b6cdebce002f2fcf5664f53747e5ba69bcbddbdf
unknown <william.best@Win10Dev.corp.gds.jeppesen.com>
2015-05-26 07:04:09 -0600

    Initial commit

---
+++ source/testgui/testgui.cpp
@@ -0,0 +1,14 @@
+// testgui.cpp : Defines the entry point for the application.
+//
+
+#include "pch.h"
+#include "testgui.h"
+
+#ifdef _MSC_VER
+#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
+#endif
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
